7db79a7ba7b0a04eda73765ed771c93233685a47
Arthur Bit-Monnot <arthur.bitmonnot@gmail.com>
2010-10-16 01:02:58 +1100

    Some love and documentation for the new classes

--- src/com/jtorrentapi/TorrentInfo.java
+++ src/com/jtorrentapi/TorrentInfo.java
@@ -1,29 +1,66 @@
+/*
+ * Java Bittorrent API as its name indicates is a JAVA API that implements the Bittorrent Protocol
+ * This project contains two packages:
+ * 1. jBittorrentAPI is the "client" part, i.e. it implements all classes needed to publish
+ *    files, share them and download them.
+ *    This package also contains example classes on how a developer could create new applications.
+ * 2. trackerBT is the "tracker" part, i.e. it implements a all classes needed to run
+ *    a Bittorrent tracker that coordinates peers exchanges. *
+ *
+ * Copyright (C) 2007 Baptiste Dubuis, Artificial Intelligence Laboratory, EPFL
+ *
+ * This file is part of jbittorrentapi-v1.0.zip
+ *
+ * Java Bittorrent API is free software and a free user study set-up;
+ * you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Bittorrent API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Java Bittorrent API; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version 1.0
+ * @author Arthur Bit-Monnot
+ * To contact the author:
+ * email: athanare@gmail.com
+ *
+ * More information about Java Bittorrent API:
+ *    http://sourceforge.net/projects/bitext/
+ */
+
 package com.jtorrentapi;
 
+
+/**
+ * Stores general informations about the torrent.
+ * @author Arthur Bit-Monnot
+ *
+ */
 public class TorrentInfo {
 
-	
-	
 	private long downloaded = 0;
     private long uploaded = 0;
-    //private long left = 0;
     private long length = 0;
     private String event = "&event=started";
     private int listeningPort = 6881;
     
-    public TorrentInfo() {
-		
-	}
-    
+    /**
+     * Constructs a TorrentInfo
+     * @param downloaded The amount of data downloaded until now 
+     * @param uploaded The amount of data uploaded until now 
+     * @param length The total length of the torrent files
+     */
     public TorrentInfo(long downloaded, long uploaded, long length) {
     	this.downloaded = downloaded;
     	this.uploaded = uploaded;
     	this.length = length;
-    }
-    
-    public String getStats() {
-    	return new String("Dl : "+downloaded+ "- Ul : "+uploaded
-    			+" - Left :" +(length-downloaded)+" - Length : "+length);
     }
 
     /**
